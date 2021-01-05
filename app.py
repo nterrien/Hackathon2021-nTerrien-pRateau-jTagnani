c2445e01c94ba9808348e8c9ba8f296ce680ef3b
nterrien <nicolas.terrien@imt-atlantique.net>
2021-01-05 16:35:29 +0100

    Ajout d'une page qui genere des mot aléatoire (pour montrer qu'on sais connecter a du code python donc)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 import flask  # BSD License (BSD-3-Clause)
 from forms.hello_form import HelloForm
+from forms.randomWord_form import NumberWordForm
+from src.calcul import randomWords
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -16,22 +18,34 @@ def home():
     form = HelloForm()
     if form.validate_on_submit():
         name = form.name.data
-        return flask.redirect(flask.url_for('helloW', name = name))
+        return flask.redirect(flask.url_for('helloW', name=name))
     else:
         return flask.render_template("home.html.jinja2", form=form)
+
 
 @app.route('/hello/<name>')
 def helloW(name):
     return flask.render_template("hello.html.jinja2", name=name)
+
 
 @app.route('/about')
 def about():
     return flask.render_template("about.html.jinja2")
 
 
+@app.route('/word', methods=["GET", "POST"])
+def word():
+    form = NumberWordForm()
+    if form.validate_on_submit():
+        words = randomWords(form.number.data)
+        return flask.render_template("word.html.jinja2", form=form, words=words)
+    return flask.render_template("word.html.jinja2", form=form)
+
+
 @app.errorhandler(404)
 def not_found(e):
     return flask.render_template("404.html.jinja2"), 404
+
 
 if __name__ == '__main__':
     app.run()
