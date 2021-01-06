4a4ffbf1012692fe7acea126f4aaa076f7058fcd
nterrien <nicolas.terrien@imt-atlantique.net>
2021-01-06 14:34:38 +0100

    Création d'une page pour upload un fichier (qui devient l'icone du site)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, flash, request, redirect, url_for
 import flask  # BSD License (BSD-3-Clause)
+import os
+from werkzeug.utils import secure_filename
 from bdd.database import db, init_database, populate_database, clear_database
 from bdd.dbMethods import addVisitor, findAllVisitor
 from forms.hello_form import HelloForm
@@ -50,11 +52,34 @@ def word():
 def visitors_list():
     return flask.render_template("visitors_list.html.jinja2", visitors=findAllVisitor())
 
+
 @app.route('/reset')
 def reset():
     clear_database()
     init_database()
     return flask.redirect(flask.url_for('home'))
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        print(file.filename)
+        if file and '.' in file.filename and file.filename .rsplit('.', 1)[1] == 'ico':
+            filename = "favicon.ico"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return flask.redirect(flask.url_for('home'))
+    return flask.render_template("upload_favico.html.jinja2")
+
 
 @app.errorhandler(404)
 def not_found(e):
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 SECRET_KEY = os.urandom(32)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///bdd/bdd.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+UPLOAD_FOLDER = 'static/'
