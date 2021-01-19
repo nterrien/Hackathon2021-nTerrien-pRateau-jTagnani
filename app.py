1a73993ebcbbb5995001a7fff87e8a5e3c66cb01
nterrien <nicolas.terrien@imt-atlantique.net>
2021-01-19 09:22:52 +0100

    Archivage des pages crées precedemment dans l'architecture

--- app.py
+++ app.py
@@ -21,84 +21,8 @@ with app.app_context():
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    username = None
-    if 'user' in session:
-        user = session['user']
-        visitor = findVisitorById(user)
-        if visitor: username = visitor.name
-    form = HelloForm()
-    if form.validate_on_submit():
-        name = form.name.data.lower().replace('<', '').replace('>', '').replace(
-            '/', '').replace('\\', '').replace('&', '').replace('"', '').replace("'", '')
-        id = saveNameInfo(name)
-        session['user'] = id
-        return flask.redirect(flask.url_for('helloW', name=name))
-    else:
-        return flask.render_template("home.html.jinja2", form=form, username=username)
+    return flask.render_template("home.html.jinja2")
 
-
-@app.route('/hello/<name>')
-def helloW(name):
-    usages = findNameUsage(name)
-    return flask.render_template("hello.html.jinja2", name=name, usages=usages)
-
-
-@app.route('/about')
-def about():
-    return flask.render_template("about.html.jinja2")
-
-
-@app.route('/word', methods=["GET", "POST"])
-def word():
-    form = NumberWordForm()
-    if form.validate_on_submit():
-        words = randomWords(form.number.data)
-        return flask.render_template("word.html.jinja2", form=form, words=words)
-    return flask.render_template("word.html.jinja2", form=form)
-
-
-@app.route('/visitors')
-def visitors_list():
-    user = None
-    if 'user' in session:
-        user = session['user']
-    return flask.render_template("visitors_list.html.jinja2", visitors=findAllVisitor(), user=user)
-
-
-@app.route('/reset')
-def reset():
-    clear_database()
-    init_database()
-    if 'user' in session : session.pop('user')
-    return flask.redirect(flask.url_for('home'))
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('Erreur : Pas de fichier')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('Erreur : Pas de fichier')
-            return redirect(request.url)
-        if file and '.' in file.filename and file.filename.rsplit('.', 1)[1] == 'ico':
-            filename = "favicon.ico"
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return flask.redirect(flask.url_for('home'))
-        else:
-            if '.' in file.filename:
-                flash("Erreur : l'extension est " +
-                    file.filename .rsplit('.', 1)[1] +" au lieu de .ico")
-                return redirect(request.url)
-            else:
-                flash("Erreur : le fichier n'a pas d'extension, or l'extension dois être .ico")
-                return redirect(request.url)
-    return flask.render_template("upload_favico.html.jinja2")
 
 
 @app.errorhandler(404)
