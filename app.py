53ca2c6f11e6daa745a3d97a9308acf938b813e7
JT-92 <57745836+JT-92@users.noreply.github.com>
2021-01-19 13:30:17 +0100

    ajout de login
    ne marche pas encore, probleme au niveau de sign in

--- app.py
+++ app.py
@@ -1,21 +1,83 @@
 from flask import Flask, flash, request, redirect, url_for, make_response, session
+from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 import flask  # BSD License (BSD-3-Clause)
 import os
+import hashlib
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+
+
 #db.init_app(app)
 
 # with app.app_context():
 #     init_database()
 
-
+# Route de base qui conduit à Login si l'utilisateur n'est pas identifié et à home si il l'est
 @app.route('/', methods=["GET", "POST"])
 def home():
-    return flask.render_template("home.html.jinja2")
+    if not session.get('logged_in'):
+        return flask.render_template('login.html.jinja2')
+    else:
+        return flask.render_template("home.html.jinja2")
+
+# Page de login
+@app.route('/login', methods=['GET', 'POST'])
+def do_admin_login():
+    if (request.form['password'] == 'password' and request.form['username'] == 'admin'):
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+# logout
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+# signin
+@app.route("/signin")
+def signin():
+    result = request.form
+    u = result['username']
+    p = result['password']
+    b = p.encode('utf-8')
+    salt = os.urandom(16)
+    hash = hashlib.pbkdf2_hmac('sha256', b, salt, 100000)
+    return flask.render_template('signin.html.jinja2')
+
+@app.route("/isSigned",  methods=["POST"])
+def isSigned():
+    result = request.form
+    u = result['username']
+    p = result['password']
+    b = p.encode('utf-8')
+    salt = os.urandom(16)
+    hash = hashlib.pbkdf2_hmac('sha256', b, salt, 100000)
+    if False:
+        session['logged_in']=True
+    else :
+        session['logged_in']=False
+    return flask.url_for('home')
+
+class MyForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+
+@app.route('/general', methods=["GET", "POST"])
+def general():
+    if not session.get('logged_in'):
+        return flask.render_template('login.html.jinja2')
+    else:
+        return flask.render_template("general.html.jinja2")
+    
 
 @app.errorhandler(404)
 def not_found(e):
@@ -23,4 +85,5 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.secret_key = os.urandom(16)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
