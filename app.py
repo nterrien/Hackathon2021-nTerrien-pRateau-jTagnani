8c892435b86d4ab9d8165ce3bb54170a9ba7a18c
nterrien <nicolas.terrien@imt-atlantique.net>
2021-01-19 09:30:02 +0100

    oubli de supprimer ceratines info

--- app.py
+++ app.py
@@ -2,28 +2,20 @@ from flask import Flask, flash, request, redirect, url_for, make_response, sessi
 import flask  # BSD License (BSD-3-Clause)
 import os
 from werkzeug.utils import secure_filename
-from bdd.database import db, init_database, populate_database, clear_database
-from bdd.dbMethods import findAllVisitor, findNameUsage, findVisitorById
-from api.nameAPI import saveNameInfo
-from forms.hello_form import HelloForm
-from forms.randomWord_form import NumberWordForm
-from src.calcul import randomWords
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-db.init_app(app)
+#db.init_app(app)
 
-with app.app_context():
-    init_database()
+# with app.app_context():
+#     init_database()
 
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     return flask.render_template("home.html.jinja2")
-
-
 
 @app.errorhandler(404)
 def not_found(e):
